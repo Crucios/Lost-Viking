0286d42afe632dd5ac1f12277f336f3a27fa344a
cocphilips <57974352+cocphilips@users.noreply.github.com>
2019-12-07 13:04:09 +0700

    Map Hospital Added

--- core/src/com/mygdx/lifeisagame/LifeIsAGame.java
+++ core/src/com/mygdx/lifeisagame/LifeIsAGame.java
@@ -1,20 +1,23 @@
 package com.mygdx.lifeisagame;
 
+import com.badlogic.gdx.files.*;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.*;
 
 public class LifeIsAGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img,img1;
-	
+	Texture babiesTexture;
+	Sprite babies;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		img1= new Texture("Babies.png");
+		FileHandle babiesFileHandle = Gdx.files.internal("Player/Babies.png"); 
+		babiesTexture= new Texture(babiesFileHandle);
+		babies=new Sprite(babiesTexture,0,0,32,64);
 	}
 
 	@Override
@@ -22,14 +25,12 @@ public class LifeIsAGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.draw(img1,10,10);
+		batch.draw(babies,0,0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
